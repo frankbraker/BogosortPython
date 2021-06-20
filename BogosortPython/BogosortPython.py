8357cebc00bf96afa0810c0affdc2917817071f6
Frank Braker <frankbraker@gmail.com>
2021-06-19 21:01:31 -0600

    initial commit

---
+++ BogosortPython/BogosortPython.py
@@ -0,0 +1,37 @@
+from random import seed
+from random import randint
+from bogoSorter import BogoSorter as bs
+import time
+import sys
+
+
+if (len(sys.argv) == 1 or len(sys.argv) < 5 ):
+    print(f"\n   usage: python {sys.argv[0]} [seed elementsCount minVal maxVal].\n\nFailing to provide all 4 arguments results in the following queries:\n")
+    s = int(input("Seed : "))
+
+    n = int(input("Enter number elements to generate : "))
+    minVal = int(input("Enter minimum allowed value for all elements : "))
+    maxVal = int(input("Enter maximum allowed value for all elements : "))
+else:
+    s = int(sys.argv[1])
+    n = int(sys.argv[2])
+    minVal = int(sys.argv[3])
+    maxVal = int(sys.argv[4])
+
+seed(s)
+
+a = []
+
+for _ in range(n):
+    a.append(randint(minVal,maxVal))
+
+sorter = bs(a)
+
+print("\nList is - ", a)
+print("\nSorted list is - ")
+
+startTime = time.time()
+sortedlist = sorter.doSort()
+print (f"elapsed Time = {time.time() - startTime}" )
+print(sortedlist)
+
---
+++ BogosortPython/bogoSorter.py
@@ -0,0 +1,43 @@
+from random import randint
+
+class BogoSorter(object):
+
+    array = []
+
+    # BogoSorter class takes an array and creates an output array that is sorted
+    def __init__ (self, input):
+        self.array = input
+
+    def isSorted(self):
+        a = self.array.copy()
+        
+        a.sort()
+        if (a == self.array):
+            return True
+        else:
+            return False
+
+    def doSort(self):
+        len_array = len(self.array)
+
+        while (False == self.isSorted()):
+
+            a = self.array.copy()
+            b = []
+
+            while len(a) > 0 :
+                r = randint(0,len(a)-1)
+                b.append(a.pop(r))
+            #print(b)
+            self.array = b.copy()            
+
+        #if (self.isSorted()):
+        #    print("it worked!")
+        #else:
+        #    print("it's still not sorted")
+
+        return self.array
+
+
+
+
